36b2d14025b6ac2b2de1bd99766df3ea7b5b75fb
alubo23 <alvinl@uninorte.edu.co>
2022-09-21 09:48:42 -0500

    Adelanto

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('login.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 5000, debug = True)
