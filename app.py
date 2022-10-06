8068b43ff601dcdc753e049084a0e42139eb1c54
alubo23 <alvinl@uninorte.edu.co>
2022-10-05 21:53:03 -0500

    Upgrade V6 Elian

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from datetime import date, datetime
-from flask import Flask, render_template, url_for, request, redirect,flash
+from flask import Flask, render_template, url_for, request, redirect, flash
 import controlador
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app=Flask(__name__)
 
@@ -10,42 +11,100 @@ app.secret_key='Mi clave Secreta'+str(datetime.now)
 def prueba():
     return True
 
+
+@app.route('/activarCuenta', methods=['POST'])
+def activarCuenta():
+    datos=request.form
+    Usuario=datos['Usuario']
+    Cod_Verificacion=datos['Codigo']
+    if Usuario=='' and Cod_Verificacion=='':
+        flash('Datos Incompletos')
+        return redirect('/validar')
+    else:
+        resultado=controlador.activar_cuenta(Usuario, Cod_Verificacion)
+        if resultado==True:
+            flash('Cuenta Activada Safisfactoriamente')
+            return redirect('/login')   
+        else:
+            flash('Error en la activación de la activación')
+            return redirect('/validar')
+              
+                       
+@app.route('/verificar', methods=['POST'])
+def validarlogin():
+    datos=request.form
+    Usu=datos['Usuario']
+    Contraseña=datos['Contraseña']
+    if Usu==''and Contraseña=='':
+       flash('Datos Incompletos')
+       print('Datos Incompletos')
+       return redirect(url_for('login'))
+    elif len(Contraseña)<=6:    
+       flash('La contraseña debe tener minimo 6 caracteres')
+       return redirect(url_for('login'))
+    else:
+        resultado=controlador.validacion_login(Usu)
+        if resultado==False:
+            flash('Usuario/Contraseña Incorrectos')
+            return redirect(url_for('login'))
+        else:
+            if check_password_hash(resultado[0]['Contraseña'],Contraseña):
+                if resultado[0]['Verificado']==1:
+                    return render_template('bandeja.html')
+                else:
+                    return redirect(url_for('validar'))
+            else:
+                flash('Contraseña Incorrecta')
+                return redirect(url_for('login'))
+            
+
 @app.route('/addregistro', methods=['POST'])
 def add_registro():
     datos=request.form
-    nom=datos['nombre']
-    ape=datos['apellido']
-    usu=datos['email']
-    p1=datos['pass1']
-    p2=datos['pass2']
-     
-  
-
-    if nom==''and ape=='' and usu=='' and p1=='' and p2=='':
-        #return '<h2>Datos Incompletos</h2>'
+    Usuario=datos['Usuario']
+    Contraseña=datos['Contraseña']
+    Email=datos['Email']
+    ContraseñaEnc=generate_password_hash(Contraseña)
+    if Usuario==''and Contraseña=='' and Email=='':
        flash('Datos Incompletos')
-    elif p1!=p2:
-        #return '<h2>Las Contraseñas no coinciden</h2>'
-       flash('Las Constraseñas no Coinciden')
-    elif len(p1)<8:
-       #return '<h2>Verificar las constraseñas</h2>'     
-       flash('Verificar Tamaño de la Contaseña')
+       print('Datos Incompletos')
+       return redirect(url_for('registro'))
+    elif len(Contraseña)<=6:    
+       flash('La contraseña debe tener minimo 6 caracteres')
+       return redirect(url_for('registro'))
     else:
-       resultado=controlador.adicionar_registros(nom,ape,usu,p1)
+       resultado=controlador.adicionar_registros(Usuario,ContraseñaEnc,Email)
        if resultado:
         flash('Registro Almacenado Correctamente')
+        return redirect(url_for('login'))
        else:
-        flash('Error en Base de Datos')
+        flash('Ha oc')
+        return redirect(url_for('registro'))
+    
+    
+@app.route('/recuperarcontra', methods=['POST'])
+def recuperarcontra():
+    datos=request.form
+    Email=datos['Email']
+    if  Email=='':
+        flash('Ingrese un correo') 
+        return redirect(url_for('recuperar'))    
+    else:
+        resultado = controlador.validarcorreo(Email)
+        if resultado == 'NO':
+            flash('No hay una cuenta asociada a este correo')
+            return redirect(url_for('recuperar')) 
+        elif resultado == 'SI':
+            flash('Revise la bandeja de entrada de su correo')
+            return redirect(url_for('login')) 
+        else:
+            flash('Invalid')
+            return redirect(url_for('recuperar'))
 
-    return redirect(url_for('registro'))
-
-
-
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
+    
+@app.route('/bandeja')
+def bandeja():
+    return render_template('bandeja.html')
 
 @app.route('/login')
 def login():
@@ -55,17 +114,21 @@ def login():
 def registro():
     return render_template('registro.html')
 
-@app.route('/contra')
-def contra():
-    return render_template('contra.html')
+@app.route('/nueva-contra')
+def nueva_contra():
+    return render_template('nueva-contra.html')
 
 @app.route('/recuperar')
 def recuperar():
     return render_template('recuperar.html')
 
-@app.route('/productos')
-def productos():
-    return render_template('productos.html')
+@app.route('/validar')
+def validar():
+    return render_template('validar.html')
+
+@app.route('/politicas')
+def politicas():
+    return render_template('politicas.html')
 
 
 if  __name__=='__main__':
--- controlador.py
+++ controlador.py
@@ -1,5 +1,90 @@
+from ctypes import resize
+from dataclasses import replace
+from datetime import datetime
+import email
+import sqlite3
+import EnviarCorreo
+from flask import flash
+import random
 
 
-def adicionar_registros(nombre,apellido,usuario,p1):
+DB_NAME='Ejemplo_DB.db'
 
-    return False
+def conexion():
+    conn=sqlite3.connect(DB_NAME)
+    return conn
+
+def adicionar_registros(Usuario, Contraseña, Email):
+    may = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    num = "0123456789"
+    general = may + num 
+    longitud = 6
+    codigo = random.sample(general, longitud)
+    Cod_verificacion = "".join(codigo)
+    try:
+        db=conexion()
+        cursor=db.cursor()
+        sql = 'INSERT INTO Usuario (Usuario, Contraseña, Email, Verificado, Cod_Verificacion) VALUES (?,?,?,?,?)'
+        cursor.execute(sql,[Usuario, Contraseña, Email, 0, Cod_verificacion])
+        db.commit()
+        EnviarCorreo.Correo(Email, Cod_verificacion)
+        return True
+    except:
+        return False
+    
+def validacion_login(Usuario):
+    try:   
+        db=conexion()
+        cursor=db.cursor()
+        sql = 'SELECT * FROM Usuario WHERE Usuario=?'
+        cursor.execute(sql,[Usuario])
+        resultado = cursor.fetchone()
+        print(resultado)
+        datos=[
+            {
+                'ID':resultado[0],
+                'Usuario':resultado[1],
+                'Contraseña':resultado[2],
+                'Email':resultado[3],
+                'Verificado':resultado[4],
+                'Cod_Verificacion':resultado[5]       
+            }       
+        ]
+        return datos
+    except:
+        return False
+    
+def activar_cuenta(Usuario, Cod_Verificacion):
+    try:   
+        db=conexion()
+        cursor=db.cursor()
+        #sql='UPDATE Usuario SET Verificado=1 WHERE Usuario=? AND Cod_Verificacion=?'
+        #
+        Sql = "UPDATE Usuario SET Verificado=1 WHERE Usuario=? AND Cod_Verificacion=?"
+        cursor.execute(Sql,[Usuario, Cod_Verificacion])
+        db.commit()
+
+        print(Usuario, Cod_Verificacion)
+        return True
+    except:
+        return False
+    
+    
+def validarcorreo(Email):
+    print(Email)
+    try:   
+        db=conexion()
+        cursor=db.cursor()
+        Sql = 'SELECT * FROM Usuario WHERE Email=?'
+        cursor.execute(Sql,[Email])
+        resultado = cursor.fetchone()
+        EnviarCorreo.RecuperarContraseña()
+        print(resultado)
+        if resultado != None:
+            return 'SI'
+        else: 
+            return 'NO'
+    except:
+        return False
+    
+    
